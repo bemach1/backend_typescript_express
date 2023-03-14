817c0e70513afc564f638f86ea80ee8136c8fe81
Cleyton <professor.cleyton.mach1@gmail.com>
2023-03-14 20:11:31 -0300

    Primeira aula de backend

---
+++ node-com-js/index.js
@@ -0,0 +1,10 @@
+// Console que indica inicio da execução
+console.log('Iniciou o programa')
+// Importo o modulo que instalei via npm
+const prompt = require('prompt-sync')()
+// Uso um método do meu módulo para receber o nome da pessoa
+let nomePessoa = prompt('Digite o nome da pessoa')
+// Exibo o nome da pessoa
+console.log(nomePessoa)
+// Indico o fim da execução do meu script
+console.log('Finalizou o programa')
