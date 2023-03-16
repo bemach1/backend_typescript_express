8c9b52f017e9c7b445d191507a1dd1347c18b8f2
Cleyton <professor.cleyton.mach1@gmail.com>
2023-03-16 20:52:21 -0300

    Aula de tipos typescript

--- types_typescript/src/index.ts
+++ types_typescript/src/index.ts
@@ -2,75 +2,103 @@
 
 // Tipo Any
 let anyVar: any = 'Teste';
-console.log('tipo Any', anyVar);
+// console.log('tipo Any', anyVar);
 anyVar = 5;
-console.log('Tipo any', anyVar);
+//console.log('Tipo any', anyVar);
 
-// Tipo boleano
+// // Tipo boleano
 let booleanVar: boolean = false;
 
-// Tipos de número
+// // Tipos de número
 let decimal: number = 6;
-console.log('Tipo Decimal', decimal);
+// console.log('Tipo Decimal', decimal);
 let hex: number = 0xf00d;
-console.log(hex);
+// console.log('Tipo hexadecimal',hex);
 let binary: number = 0b1010;
-console.log(binary);
+// console.log('Tipo Binario',binary);
 let octal: number = 0o744;
-console.log(octal);
+// console.log('Tipo octal',octal);
 let big: bigint = 9007199254740991n;
-console.log(big);
-//Tipo string ou texto
-let stringVar: string = "Texto";
-console.log(stringVar);
+// console.log('tipo bigint',big);
 
-// Tipo array 
+// //Tipo string ou texto
+let stringVar: string = 'Texto' + 4;
+// console.log(stringVar);
+
+// // Tipo array 
 let arrayVar: number[] = [1, 2, 3];
-console.log(arrayVar);
+//console.log(arrayVar);
 arrayVar.push(5);
-console.log(arrayVar);
+//console.log(arrayVar);
 
-// Outra maneira de escrever
+// // Outra maneira de escrever
 let arrayVarOuther: Array<number> = [1, 2, 3];
-console.log(arrayVarOuther);
+//console.log(arrayVarOuther);
 
-// Tipo Tuple ou array Tuple
-let tupleVar: [string, number];
+// // Tipo Tuple ou array Tuple
+let pessoaIdade: [string, number];
 // Atribuindo valores por posição
-tupleVar = ["hello", 10];
-console.log(tupleVar);
+pessoaIdade = ['Cleyton', 32];
+// console.log(pessoaIdade);
 
-// Tipo Enum ou Enumerador
+// // Tipo Enum ou Enumerador
 enum EnumVar {
-    aprovado,
-    reprovado,
-    recuperacao,
+    aprovada=250,  
+    reprovada=100, 
+    emAnalise=300, 
+    pix=400
 }
-console.log(EnumVar.aprovado);
-console.log(EnumVar.recuperacao);
-console.log(EnumVar[1]);
+// console.log(EnumVar); // Posição
+// console.log(EnumVar.recuperacao);
+// console.log(EnumVar[1]); // Valor 
 
-// Tipo object ou objeto
+// // Tipo object ou objeto
 let varObject = {
     nome: 'Cleyton',
-    idade: 25
+    idade: 25,
+    novaPropriedade:''
 }
-
+varObject.novaPropriedade = 'x'
 console.log(varObject);
 
-// Usando interface
-interface User {
+// // Usando interface
+interface IAddress {
+    street:string;
+    number: number;
+    city:string;
+    state:string;
+}
+
+interface IUser {
     name: string;
     age: number;
     email: string;
-    isAdmin: boolean;
+    isAdmin?: boolean; // Propriedade opcional
+    address?:IAddress;
 }
 
-let user:User = {
+let user:IUser = {
     name:'Cleyton',
     age:32,
     email:'professor.cleyton.mach1@gmail.com',
-    isAdmin:false
+    address:{
+        street:'Rua teste',
+        number:123,
+        city:'Osasco',
+        state:'SP'
+    }
 }
 
 console.log(user)
+
+
+
+/* Explicação de spread operator */
+
+let listaDeConvidadosEspeciais: Array<string> = 
+['Pablo', 'Geovana', 'Fabio'];
+let convidadosGeral:Array<string> = [...listaDeConvidadosEspeciais];
+convidadosGeral.push('Paulo');
+convidadosGeral.push('Pedro');
+// console.log(listaDeConvidadosEspeciais);
+// console.log(convidadosGeral);
