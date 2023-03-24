e2cab48b4f69d2500e853a94a7fc9cce790ef281
Cleyton <professor.cleyton.mach1@gmail.com>
2023-03-24 20:21:03 -0300

    Aula

--- express-first-api/src/server.ts
+++ express-first-api/src/server.ts
@@ -1,23 +1,29 @@
-// Importando o framwork
 import express from 'express';
-// Importando o que eu quero utilizar
 import { Router, Request, Response } from 'express';
+import {IStudent} from './interfaces/student';
+import getLastId from './utils/getLastId';
 
-const students = [
-{ "id":100, "name": "Cleyton", "age": 32 },
-{ "id":101,"name": "Fabio", "age": 31 }, 
-{ "id":102,"name": "Pablo", "age": 42 }
+
+const students: IStudent[] = [
+    { "id": 100, "name": "Cleyton", "age": 32, "cpf": "12345678912" }
 ];
 
 enum messages {
     studentNotFind = 'Estudante não encontrado',
-    studentNotSet = 'Estudante não cadastrado'
+    studentNotSet = 'Estudante não cadastrado',
+    studentRegisterSuccess = 'Estudante cadastrado com sucesso'
 }
 
-function getLastId():number{
-    let item = students[students.length - 1];
-    return item.id;
+function handleBodyRegister(returnAPI: any, idStudent: number): IStudent {
+    const newStudent = {
+        id: idStudent,
+        name: returnAPI.name,
+        age: returnAPI.age,
+        cpf: returnAPI.cpf,
+    }
+    return newStudent;
 }
+
 const app = express();
 const route = Router();
 app.use(express.json());
@@ -34,20 +40,32 @@ route.get('/studentList', (req: Request, res: Response) => {
 // Retornar um estudante específico através do ID
 route.get('/student/:id', (req: Request, res: Response) => {
     let id = Number(req.params.id);
-    let student:any = students.find(item => item.id == id);
-    if (student){
+    let student: any = students.find(item => item.id == id);
+    if (student) {
         res.json(student);
-    }else{
-        res.json({message:messages.studentNotFind});
+    } else {
+        res.json({ message: messages.studentNotFind });
     }
 })
 
 route.post('/studentRegister', (req: Request, res: Response) => {
-    let student = req.body;
-    let lastID = getLastId() + 1;
-    student.id = lastID;
-    students.push(student);
-    res.json({message:messages.studentNotSet});
+    // Como valido o Body enviado?
+    if (req.body.name && req.body.age && req.body.cpf) {
+        let lastID = getLastId(students);
+        let student = handleBodyRegister(req.body, lastID);
+        students.push(student);
+        res.json({ message: messages.studentRegisterSuccess });
+
+    } else {
+        res.json({
+            message: messages.studentNotSet,
+            bodyExpected: {
+                name: 'string',
+                age: 'number',
+                cpf: 'string'
+            }
+        });
+    }
 })
 
 // Setando uso da rota ou das rotas
