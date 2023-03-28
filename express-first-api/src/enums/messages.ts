4b218e2d666983d2ebe948c5f512d14f155a8b14
Cleyton <professor.cleyton.mach1@gmail.com>
2023-03-28 20:39:59 -0300

    Adicionando incremento!

--- express-first-api/src/enums/messages.ts
+++ express-first-api/src/enums/messages.ts
@@ -1,6 +1,6 @@
 enum messages {
     studentNotFind = 'Estudante não encontrado',
-    studentNotSet = 'Estudante não cadastrado',
+    studentNotSet = 'Estudante não cadastrado!',
     studentRegistrerSuccess = 'Estudante cadastrado com sucesso!',
     studentRegisterRemove = 'Estudante removido com sucesso!',
 }
