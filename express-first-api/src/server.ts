4fd3db5a7f1aaf7a0fad28cde25d3d0a29e8797b
Geovana Framil <geovanaframil@hotmail.com>
2023-03-29 19:39:21 -0300

    Cria função para atualizar estudante no método put

--- express-first-api/src/server.ts
+++ express-first-api/src/server.ts
@@ -1,9 +1,9 @@
-import express from 'express';
-import { Router, Request, Response } from 'express';
-import { IStudent } from './interfaces/student';
-import getLastId from './utils/utilgetLastid';
-import messages from './enums/messages'
-import saveDataInJson from './utils/saveDataInJson';
+import express from "express";
+import { Router, Request, Response } from "express";
+import { IStudent } from "./interfaces/student";
+import getLastId from "./utils/utilgetLastid";
+import messages from "./enums/messages";
+import saveDataInJson from "./utils/saveDataInJson";
 
 const data = require("../database/students.json");
 const students: IStudent[] = data;
@@ -39,7 +39,7 @@ function verifyBody(body: any): { isValid: boolean; message: string } {
     };
   }
 
-  if (!body.cpf || (typeof body.cpf !== "string" || body.cpf.length !== 11)) {
+  if (!body.cpf || typeof body.cpf !== "string" || body.cpf.length !== 11) {
     return {
       isValid: false,
       message:
@@ -72,122 +72,62 @@ route.get("/student/:id", (req: Request, res: Response) => {
   }
 });
 
-<<<<<<< HEAD
-route.post('/studentRegister', (req: Request, res: Response) => {
-    // Como valido o Body enviado?
-    if (req.body.name && req.body.age && req.body.cpf) {
-        let lastID = getLastId(students);
-        let student = handleBodyRegister(req.body, lastID);
-        students.push(student);
-        res.json({ message: messages.studentRegistrerSuccess });
-        saveDataInJson(students)
-    } else {
-        res.json({
-            message: messages.studentNotSet,
-            bodyExpected: {
-                name: 'string',
-                age: 'number',
-                cpf: 'string'
-            }
-        });
-    }
-})
-=======
 route.post("/studentRegister", (req: Request, res: Response) => {
   const body = req.body;
   const validBody = verifyBody(body);
   // Como valido o Body enviado?
-  if (validBody) {
+  if (validBody.isValid) {
     let lastID = getLastId(students);
     let student = handleBodyRegister(body, lastID);
     students.push(student);
     const studentJson = JSON.stringify(students);
     res.json({ message: messages.studentRegistrerSuccess });
-    fs.writeFileSync("./database/students.json", studentJson);
+    saveDataInJson(students);
+  } else {
+    res.json({
+      message: messages.studentNotSet,
+    });
   }
-  //   else {
-  //     res.json({
-  //       message: messages.studentNotSet,
-  //       bodyExpected: {
-  //         name: "string",
-  //         age: "number",
-  //         cpf: "string",
-  //       },
-  //     });
-  //   }
 });
->>>>>>> 2bcaa6f (Função para validar campos passados no corpo da API)
 
 // Criando uma rota PUT para atualizar um registro no banco
 route.put("/studentUpdate/:id", (req: Request, res: Response) => {
   let id = Number(req.params.id);
+  let body = req.body;
   // Verificar se passou um ID na Rota
   let resultStudent = students.find((item) => item.id === id);
   let indexObject = students.findIndex((item) => item.id === id);
 
   if (resultStudent) {
-    // Validar dados que foram enviados no body
-    if (req.body.name || req.body.age || req.body.cpf) {
-      // ATUALIZANDO O OBJETO
-      // Name
-      students[indexObject].name = req.body.name
-        ? req.body.name
-        : students[indexObject].name;
-
-      // Age
-      students[indexObject].age = req.body.age
-        ? req.body.age
-        : students[indexObject].age;
-
-<<<<<<< HEAD
-            // CPF
-            students[indexObject].cpf = req.body.cpf ? req.body.cpf :
-                students[indexObject].cpf;
- 
-            res.json({
-                message: messages.studentRegistrerSuccess
-            });
-
-            saveDataInJson(students)         
-        } else {
-            res.json({
-                message: messages.studentNotSet,
-                bodyExpected: {
-                    name: 'string',
-                    age: 'number',
-                    cpf: 'string'
-                }
-            });
-        }
-=======
-      // CPF
-      students[indexObject].cpf = req.body.cpf
-        ? req.body.cpf
-        : students[indexObject].cpf;
-      const studentJson = JSON.stringify(students);
-      res.json({
-        message: messages.studentRegistrerSuccess,
-      });
-      fs.writeFileSync("./database/students.json", studentJson);
->>>>>>> 2bcaa6f (Função para validar campos passados no corpo da API)
-    } else {
-      res.json({
-        message: messages.studentNotSet,
+    const allowedFields = ["name", "age", "cpf"];
+    const receivedFields = Object.keys(body);
+    const invalidFields = receivedFields.filter(
+      (field) => !allowedFields.includes(field)
+    );
+    if (invalidFields.length > 0) {
+      res.send({
+        message: messages.studentNotUpdated,
         bodyExpected: {
           name: "string",
           age: "number",
           cpf: "string",
         },
       });
+      return;
     }
+    const updatedUser = {
+      ...students[indexObject],
+      ...body,
+      id: id,
+    };
+    students[indexObject] = updatedUser;
+    res.json({
+      message: messages.studentRegistrerSuccess,
+    });
+    saveDataInJson(students);
   } else {
     res.json({
       message: messages.studentNotSet,
-      bodyExpected: {
-        name: "string",
-        age: "number",
-        cpf: "string",
-      },
     });
   }
 });
@@ -201,33 +141,17 @@ route.delete("/studentDelete/:id", (req: Request, res: Response) => {
     // Filtra para gerar um novo array sem o registro setado
     let studentObject = students.filter((item) => item.id !== id);
 
-<<<<<<< HEAD
-        res.json({
-            message: messages.studentRegisterRemove
-        });
-      
-        saveDataInJson(studentObject)
-    } else{
-        res.json({
-            message: messages.studentNotFind
-        });
-    }
-
-
-})
-=======
-    const studentJson = JSON.stringify(studentObject);
     res.json({
       message: messages.studentRegisterRemove,
     });
-    fs.writeFileSync("./database/students.json", studentJson);
+
+    saveDataInJson(studentObject);
   } else {
     res.json({
       message: messages.studentNotFind,
     });
   }
 });
->>>>>>> 2bcaa6f (Função para validar campos passados no corpo da API)
 
 // Setando uso da rota ou das rotas
 app.use(route);
