5738802d790fea27ab668f3098f99eb924903943
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2023-03-29 16:13:59 -0300

    Implementa função que grava os dados no json

--- express-first-api/src/server.ts
+++ express-first-api/src/server.ts
@@ -3,8 +3,8 @@ import { Router, Request, Response } from 'express';
 import { IStudent } from './interfaces/student';
 import getLastId from './utils/getLastId';
 import messages from './enums/messages'
+import saveDataInJson from './utils/saveDataInJson';
 
-const fs = require('fs');
 const data = require('../database/students.json');
 const students: IStudent[] = data;
 const app = express();
@@ -48,10 +48,8 @@ route.post('/studentRegister', (req: Request, res: Response) => {
         let lastID = getLastId(students);
         let student = handleBodyRegister(req.body, lastID);
         students.push(student);
-        const studentJson = JSON.stringify(students);
         res.json({ message: messages.studentRegistrerSuccess });
-        fs.writeFileSync("./database/students.json", studentJson);
-
+        saveDataInJson(students)
     } else {
         res.json({
             message: messages.studentNotSet,
@@ -86,11 +84,12 @@ route.put('/studentUpdate/:id', (req: Request, res: Response) => {
             // CPF
             students[indexObject].cpf = req.body.cpf ? req.body.cpf :
                 students[indexObject].cpf;
-            const studentJson = JSON.stringify(students);
+ 
             res.json({
                 message: messages.studentRegistrerSuccess
             });
-            fs.writeFileSync("./database/students.json", studentJson);
+
+            saveDataInJson(students)         
         } else {
             res.json({
                 message: messages.studentNotSet,
@@ -122,11 +121,11 @@ route.delete('/studentDelete/:id', (req: Request, res: Response) => {
         // Filtra para gerar um novo array sem o registro setado
         let studentObject = students.filter(item => item.id !== id);
 
-        const studentJson = JSON.stringify(studentObject);
         res.json({
             message: messages.studentRegisterRemove
         });
-        fs.writeFileSync("./database/students.json", studentJson);
+      
+        saveDataInJson(studentObject)
     } else{
         res.json({
             message: messages.studentNotFind
