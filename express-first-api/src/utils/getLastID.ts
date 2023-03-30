5e6254340a7bc50c57487c6c2fa1648de8455e9d
Kayk magalhaes <kaykmagalhaesdiv@gmail.com>
2023-03-30 19:33:51 -0300

    tirando arquivo

--- express-first-api/src/utils/getLastID.ts
+++
@@ -1,8 +0,0 @@
-import { IStudents } from "../interfaces/interfaces";
-
-function getLastId(students: Array<IStudents>): number {
-    let item = students[students.length - 1];
-    return item.id;
-}
-
-export default getLastId;
