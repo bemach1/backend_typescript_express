0033f94e040c8172657c444f81a834842b6cc5e8
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2023-07-05 13:46:59 -0300

    atualizacao

--- express-first-api/src/server.ts
+++ express-first-api/src/server.ts
@@ -2,11 +2,15 @@ import express from 'express';
 import { Router, Request, Response } from 'express';
 import { IStudent } from './interfaces/student';
 import getLastId from './utils/getLastId';
-import messages from './enums/messages'
+import messages from './enums/messages';
 import saveDataInJson from './utils/saveDataInJson';
+import createDataInJson from './utils/createDataInJson';
 
-const data = require('../database/students.json');
+const data = createDataInJson([]);
+
+// const data = require('../database/students.json');
 const students: IStudent[] = data;
+
 const app = express();
 const route = Router();
 
@@ -15,8 +19,8 @@ function handleBodyRegister(returnAPI: any, idStudent: number): IStudent {
         id: idStudent,
         name: returnAPI.name,
         age: returnAPI.age,
-        cpf: returnAPI.cpf,
-    }
+        cpf: returnAPI.cpf
+    };
     return newStudent;
 }
 
@@ -24,12 +28,12 @@ app.use(express.json());
 // Rota onde a minha API vai responder
 route.get('/', (req: Request, res: Response) => {
     res.json({ message: 'Seja bem vindo a mach1' });
-})
+});
 
 // Uma lista de estudantes
 route.get('/studentList', (req: Request, res: Response) => {
     res.json(students);
-})
+});
 
 // Retornar um estudante específico através do ID
 route.get('/student/:id', (req: Request, res: Response) => {
@@ -40,7 +44,7 @@ route.get('/student/:id', (req: Request, res: Response) => {
     } else {
         res.json({ message: messages.studentNotFind });
     }
-})
+});
 
 route.post('/studentRegister', (req: Request, res: Response) => {
     // Como valido o Body enviado?
@@ -49,7 +53,7 @@ route.post('/studentRegister', (req: Request, res: Response) => {
         let student = handleBodyRegister(req.body, lastID);
         students.push(student);
         res.json({ message: messages.studentRegistrerSuccess });
-        saveDataInJson(students)
+        saveDataInJson(students);
     } else {
         res.json({
             message: messages.studentNotSet,
@@ -60,7 +64,7 @@ route.post('/studentRegister', (req: Request, res: Response) => {
             }
         });
     }
-})
+});
 
 // Criando uma rota PUT para atualizar um registro no banco
 route.put('/studentUpdate/:id', (req: Request, res: Response) => {
@@ -74,22 +78,25 @@ route.put('/studentUpdate/:id', (req: Request, res: Response) => {
         if (req.body.name || req.body.age || req.body.cpf) {
             // ATUALIZANDO O OBJETO
             // Name
-            students[indexObject].name = req.body.name ? req.body.name :
-                students[indexObject].name;
+            students[indexObject].name = req.body.name
+                ? req.body.name
+                : students[indexObject].name;
 
             // Age
-            students[indexObject].age = req.body.age ? req.body.age :
-                students[indexObject].age;
+            students[indexObject].age = req.body.age
+                ? req.body.age
+                : students[indexObject].age;
 
             // CPF
-            students[indexObject].cpf = req.body.cpf ? req.body.cpf :
-                students[indexObject].cpf;
- 
+            students[indexObject].cpf = req.body.cpf
+                ? req.body.cpf
+                : students[indexObject].cpf;
+
             res.json({
                 message: messages.studentRegistrerSuccess
             });
 
-            saveDataInJson(students)         
+            saveDataInJson(students);
         } else {
             res.json({
                 message: messages.studentNotSet,
@@ -117,23 +124,21 @@ route.delete('/studentDelete/:id', (req: Request, res: Response) => {
     let id = Number(req.params.id);
     // Verifico se passou um ID que realmente existe no JSON
     let verifyID = students.find(item => item.id === id);
-    if (verifyID){
+    if (verifyID) {
         // Filtra para gerar um novo array sem o registro setado
         let studentObject = students.filter(item => item.id !== id);
 
         res.json({
             message: messages.studentRegisterRemove
         });
-      
-        saveDataInJson(studentObject)
-    } else{
+
+        saveDataInJson(studentObject);
+    } else {
         res.json({
             message: messages.studentNotFind
         });
     }
-
-
-})
+});
 
 // Setando uso da rota ou das rotas
 app.use(route);
